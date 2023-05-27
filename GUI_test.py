749655e2de46e6e38d06ce50800c9ccd25e29ee8
ranavner <ran.avner5@gmail.com>
2023-05-27 10:08:17 +0300

    changed things in the GUI

--- GUI_test.py
+++ GUI_test.py
@@ -1,7 +1,30 @@
 import streamlit as st
 import pandas as pd
 import time
+import plotly.express as px
 
+# creating individual plots for each sensor
+def plotly(df, sensor_number):
+    y = sensor_number
+    df1 = df.tail(30)
+    plot = px.line(df1, x='TIMESTAMP', y=y)
+    st.plotly_chart(plot, use_container_width=True, theme='streamlit')
+
+# creating one plot including all sensors
+def plotly_all(df):
+    df1 = df.tail(30)
+    st.line_chart(data=df1, x='TIMESTAMP')
+
+# converting the df into CSV
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+def create_snow():
+    st.snow
+
+# streamllit configuration
 st.set_page_config(
     page_title="Real-Time Data Science Dashboard",
     page_icon="✅",
@@ -10,6 +33,7 @@ st.set_page_config(
 
 st.title("Agrotech Project Dashboard")
 
+# first appearance - sensors current measurments
 Sensor1, Sensor2, Sensor3, Sensor4, Sensor5, Sensor6 = st.columns(6)
 df = pd.read_csv(('fixed2.csv'))
 Sensor1.metric(
@@ -43,14 +67,45 @@ Sensor6.metric(
     value=df['sensor6'].iloc[-1]
 )
 
-col1, col2 = st.columns(2)
+# plotting all sensors into one plot
+plotly_all(df)
+
+# creating the download button
+csv = convert_df(df)
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='Sensors_Data.csv',
+    mime='text/csv',
+)
+
+if st.button(label='HighTech Computing Integration Module Unit'):
+    st.snow()
+
+col1, col2, col3 = st.columns(3)
 df = pd.read_csv('fixed2.csv')
 
 with col1:
     label='graph'
-    value=st.line_chart(df)
+    value=plotly(df, 'sensor1')
 with col2:
-    label='table'
-    value=st.write(df)
+    label='grapg'
+    value=plotly(df, 'sensor2')
+with col3:
+    label='graph'
+    value=plotly(df, 'sensor3')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    label='graph'
+    value=plotly(df, 'sensor4')
+with col2:
+    label='graph'
+    value=plotly(df, 'sensor5')
+with col3:
+    label='graph'
+    value=plotly(df, 'sensor6')
+
 
 time.sleep(1)
--- csv_Creator.py
+++ csv_Creator.py
@@ -1,19 +1,18 @@
-import pandas as pd
-import numpy as np
 import csv
 import time
 import os
+from datetime import datetime
 import random
 
-header = ['sensor1', 'sensor2', 'sensor3','sensor4', 'sensor5', 'sensor6']
+header = ['TIMESTAMP', 'sensor1', 'sensor2', 'sensor3','sensor4', 'sensor5', 'sensor6']
 
 os.remove('fixed2.csv')
 
 with open('fixed2.csv', 'a') as t:
     writer = csv.writer(t)
     writer.writerow(header)
-    for seconds in range(200):
-        data = [random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10)]
+    for seconds in range(10000):
+        data = [datetime.now(), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10), random.randint(1,10)]
         writer.writerow(data)
         t.flush()
 
